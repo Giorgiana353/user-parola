6378902b264ee17156fb04b694d5d597cc90a8a7
Giorgiana353 <giorgiana.dinu@emanuel.ro>
2023-10-26 23:12:49 +0300

    user-parola

---
+++ user-parola/user-parola/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace user_parola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string usernamec = "giorgi";
+            const string parolac = "353";
+
+            bool autentificare = false;
+
+            while (!autentificare)
+            {
+                Console.WriteLine("Introduceti username: ");
+                string username = Console.ReadLine();
+
+                Console.WriteLine("Introduceti parola: ");
+                string parola = Console.ReadLine();
+
+                if (username == usernamec && parola == parolac)
+                {
+                    Console.WriteLine("Bine ai venit, " + usernamec);
+                    autentificare = true;
+                }
+                else
+                {
+                    Console.WriteLine("gresit");
+                }
+
+            }
+        }
+    }
+}
+    
